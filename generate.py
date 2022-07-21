db1b67f054ad141dd14428dddb6a514e41b346cf
adambartyzal <adambartyzal@gmail.com>
2022-07-21 19:18:39 +0200

    works, produces more or less constant speed

---
+++ generate.py
@@ -0,0 +1,45 @@
+import sys
+from datetime import datetime
+import math
+
+if (len(sys.argv) < 4):
+  sys.exit(f'Usage: python {sys.argv[0]} waypoints_file.gpx' 'start_timestamp' 'speed')
+
+waypoints_file = sys.argv[1]
+start = int(sys.argv[2])
+speed = int(sys.argv[3])
+current = start
+
+points = 0
+distance = 0
+prev_lat = 0
+prev_lon = 0
+
+with open(waypoints_file, 'r') as waypoints:
+  for line in waypoints:
+    if ('<trkpt' in line):
+      points += 1
+
+idx = 0
+
+with open('generated.gpx', 'w') as generated:
+  with open(waypoints_file, 'r') as waypoints:
+    for line in waypoints:
+      generated.write(line)
+      if ('<trkpt' in line):
+        lat = float(line[line.find('lat') + 5 : line.find('lat') + 14])
+        lon = float(line[line.find('lon') + 5 : line.find('lon') + 14])
+        distance = math.sqrt((lat - prev_lat) ** 2 + (lon - prev_lon) **2) * 550 * speed
+        print(distance)
+        prev_lat = lat
+        prev_lon = lon
+        idx += 1
+
+
+      if ('<ele' in line):
+        if (idx != 1):
+          print(idx)
+          current += distance
+        now = datetime.fromtimestamp(current).isoformat()
+        print(now)
+        generated.write('				<time>' + now + '</time>\n')
